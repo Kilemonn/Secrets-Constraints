af2d98f1ed7cb62b7cd591405add6a40d1274a76
Kilemonn <kylegonzalez2255@gmail.com>
2024-09-27 15:25:39 +0900

    Add pattern and tests.

---
+++ constraint/pattern.go
@@ -0,0 +1,38 @@
+package constraint
+
+import (
+	"fmt"
+	"regexp"
+)
+
+const (
+	all_pattern = "ALL"
+)
+
+type Pattern struct {
+	pattern string
+	regex   *regexp.Regexp
+}
+
+func NewPattern(p string) (pattern Pattern) {
+	pattern.pattern = p
+	regex, err := regexp.Compile(pattern.pattern)
+	if err != nil {
+		fmt.Printf("Failed to compile regex pattern: [%s].\n", err.Error())
+		pattern.pattern = ""
+	} else {
+		pattern.regex = regex
+	}
+	return
+}
+
+func (p Pattern) matches(input string) bool {
+	if p.pattern == all_pattern {
+		return true
+	}
+	if p.regex == nil {
+		return false
+	}
+
+	return p.regex.Match([]byte(input))
+}
---
+++ constraint/pattern_test.go
@@ -0,0 +1,35 @@
+package constraint
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPatternMatches(t *testing.T) {
+	var cases = []struct {
+		regex        string
+		expectsError bool
+		successTests []string
+		failTests    []string
+	}{
+		{"ALL", false, []string{"some random !@#$!@$12471o31 text"}, []string{}},                             // Using "ALL"
+		{"db-connection-string-.+", false, []string{"db-connection-string-998234"}, []string{"fail please"}}, // Valid regex with success and failure mathcing checks
+		{"invalid-pattern*+", true, []string{}, []string{}},                                                  // "Failing to initialise regex"
+	}
+
+	for _, c := range cases {
+		pattern := NewPattern(c.regex)
+		if c.expectsError {
+			assert.Nil(t, pattern.regex)
+		} else {
+			assert.NotNil(t, pattern.regex)
+			for _, test := range c.successTests {
+				assert.True(t, pattern.matches(test))
+			}
+			for _, test := range c.failTests {
+				assert.False(t, pattern.matches(test))
+			}
+		}
+	}
+}
